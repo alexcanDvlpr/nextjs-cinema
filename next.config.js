fe9ee099c2d2bde28ee68f5b444b499e0bb46442
Alejandro Cantón <alexcantongarcia97@gmail.com>
2021-03-10 20:43:50 +0100

    Create topRated method in TVShowService and refactor code

--- next.config.js
+++ next.config.js
@@ -2,7 +2,6 @@
 module.exports = {
     env: {
       API_KEY: process.env.API_KEY,
-      LANG: process.env.LANG,
       MOVIE_BASE_URL: process.env.MOVIEDB_BASE_URL,
       POSTERS_BASE_URL: process.env.POSTERS_BASE_URL
     },
--- services/TVShowsService.js
+++ services/TVShowsService.js
@@ -1,15 +1,10 @@
-
-var apiKey = process.env.API_KEY
-var page = 1
-var lang = process.env.API_KEY
-var base_url = process.env.MOVIEDB_BASE_URL;
-var posters_base_url = process.env.POSTERS_BASE_URL;
+import * as env from '../constants/enviroment'
 
 export async function getPopularTvShows() {
 
     let tvShows = []
 
-    const response = await fetch(`${base_url}/tv/popular?api_key=${apiKey}&language=${lang}&page=${page}`)
+    const response = await fetch(`${env.base_url}/tv/popular?api_key=${env.apiKey}&language=${env.lang}&page=${env.page}`)
     const { results } = await response.json()
 
     tvShows = results.map(tvShow => {
@@ -18,7 +13,7 @@ export async function getPopularTvShows() {
             name: tvShow.name,
             overview: tvShow.overview,
             vote_average: tvShow.vote_average,
-            poster: `${posters_base_url}${tvShow.backdrop_path}`
+            poster: `${env.posters_base_url}${tvShow.backdrop_path}`
         }
     })
     return tvShows
@@ -26,8 +21,27 @@ export async function getPopularTvShows() {
 
 export async function getTvShowById(id) {
 
-    const response = await fetch(`${base_url}/tv/${id}?api_key=${apiKey}&language=${lang}`)
+    const response = await fetch(`${env.base_url}/tv/${id}?api_key=${env.apiKey}&language=${env.lang}`)
     let data = await response.json()
 
     return data
+}
+
+export async function getTopRatedTvShows() {
+
+    let tvShows = []
+
+    const response = await fetch(`${env.base_url}/tv/top_rated?api_key=${env.apiKey}&language=${env.lang}&page=${env.page}`)
+    let { results } = await response.json()
+
+    tvShows = results.map(tvShow => {
+        return {
+            id: tvShow.id,
+            name: tvShow.name,
+            overview: tvShow.overview,
+            vote_average: tvShow.vote_average,
+            poster: (tvShow.backdrop_path !== null) ? `${env.posters_base_url}${tvShow.backdrop_path}` : null
+        }
+    })
+    return tvShows
 }
